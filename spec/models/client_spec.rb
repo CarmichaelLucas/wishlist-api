a7dbaa3dbacc75ce2809e3a4aabf4cdbdf526247
Lucas Carmichael <carmichael.lucas@outlook.com>
2021-10-12 17:26:00 -0300

    implementando teste do metodo privado

--- spec/models/client_spec.rb
+++ spec/models/client_spec.rb
@@ -4,10 +4,16 @@ RSpec.describe Client, type: :model do
   let(:client) { create(:client) }
   
   it { expect(client).to be_valid }
+  it { is_expected.to validate_presence_of(:name) }
+  it { is_expected.to validate_presence_of(:email) }
+  it { is_expected.to validate_uniqueness_of(:email).ignoring_case_sensitivity }
+  
+  it 'sends welcome email after creation' do
+    c = Client.new
+    expect(c).to receive(:send_email)
 
-  context 'Validate' do
-    it { is_expected.to validate_presence_of(:name) }
-    it { is_expected.to validate_presence_of(:email) }
-    it { is_expected.to validate_uniqueness_of(:email).ignoring_case_sensitivity }
+    c.name = 'Teste' 
+    c.email = 'teste@teste.com'
+    c.save
   end
 end
